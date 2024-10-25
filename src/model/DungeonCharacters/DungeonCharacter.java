fa328e5da4632b5180d62ac7fa6007ba41f34314
KingsGlaive42 <thomas.h.le123@gmail.com>
2024-10-24 18:23:58 -0700

    Commit new changes

--- src/model/DungeonCharacters/DungeonCharacter.java
+++ src/model/DungeonCharacters/DungeonCharacter.java
@@ -9,6 +9,16 @@ public abstract class DungeonCharacter {
     protected double myChanceToHit;
     protected double myChanceToBlock;
 
+    protected DungeonCharacter() {
+        this.myName = "";
+        this.myHitPoints = 0;
+        this.myMinDamage = 0;
+        this.myMaxDamage = 0;
+        this.myAttackSpeed = 0;
+        this.myChanceToHit = 0;
+        this.myChanceToBlock = 0;
+    }
+
     public abstract void attack(final DungeonCharacter theTarget);
 
     public abstract void takeDamage(final int theDamage);
---
+++ src/model/DungeonCharacters/Hero.java
@@ -0,0 +1,23 @@
+package model.DungeonCharacters;
+
+public class Hero extends DungeonCharacter {
+
+    public Hero(final String theName) {
+        this.myName = theName;
+    }
+
+    @Override
+    public void attack(DungeonCharacter theTarget) {
+
+    }
+
+    @Override
+    public void takeDamage(int theDamage) {
+
+    }
+
+    @Override
+    public void useSpecialSkill(DungeonCharacter theTarget) {
+
+    }
+}
