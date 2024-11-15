d949812f823c801267b1389b010497f13de45f52
Jayden Fausto <joeyfausto107@gmail.com>
2024-11-14 21:10:13 -0800

    Added floor drawings Added Door images updated player movement for wall collision added more resources for walls and floor Cleaned up DungeonGenerator.java

--- src/model/DungeonManager/Door.java
+++ src/model/DungeonManager/Door.java
@@ -9,39 +9,70 @@ import java.awt.Graphics2D;
 class Door {
     private final DoorDirection myDirection;
     private double myX, myY;
-    private static final int DOOR_WIDTH = 32;
-    private static final int DOOR_HEIGHT = 32;
+    private static final int TILE_DIMENSION = 32;
 
     Door(final DoorDirection theDirection) {
         myDirection = theDirection;
         switch (myDirection) {
             case DoorDirection.UP:
-                myX = 7.5 * DOOR_WIDTH; // Centered on top
+                myX = 8 * TILE_DIMENSION; // Centered on top
                 myY = 0;
                 break;
             case DoorDirection.DOWN:
-                myX = 7.5 * DOOR_WIDTH;
-                myY = 11 * DOOR_HEIGHT;
+                myX = 8 * TILE_DIMENSION;
+                myY = 12 * TILE_DIMENSION;
                 break;
             case DoorDirection.LEFT:
                 myX = 0;
-                myY = 5.5 * DOOR_HEIGHT;
+                myY = 6 * TILE_DIMENSION;
                 break;
             case DoorDirection.RIGHT:
-                myX = 15 * DOOR_WIDTH;
-                myY = 5.5 * DOOR_HEIGHT;
+                myX = 16 * TILE_DIMENSION;
+                myY = 6 * TILE_DIMENSION;
                 break;
         }
     }
 
     public void draw(final Graphics2D theGraphics2D) {
-        theGraphics2D.setColor(Color.BLUE);
-        theGraphics2D.fill(new Rectangle2D.Double(myX, myY, DOOR_WIDTH, DOOR_HEIGHT));
+        theGraphics2D.setColor(new Color(0x421d00));
+        switch (myDirection) {
+            case DoorDirection.UP:
+                theGraphics2D.fill(new Rectangle2D.Double(myX , myY, TILE_DIMENSION, TILE_DIMENSION));
+                break;
+            case DoorDirection.DOWN:
+                theGraphics2D.fill(new Rectangle2D.Double(myX, myY + 16, TILE_DIMENSION, (double) TILE_DIMENSION / 2));
+                break;
+            case DoorDirection.LEFT:
+                theGraphics2D.fill(new Rectangle2D.Double(myX, myY, (double) TILE_DIMENSION / 4, TILE_DIMENSION));
+                break;
+            case DoorDirection.RIGHT:
+                theGraphics2D.fill(new Rectangle2D.Double(myX + 24, myY, (double) TILE_DIMENSION / 4, TILE_DIMENSION));
+                break;
+        }
+
     }
 
     boolean isPlayerColliding(final Player thePlayer) {
-        Rectangle2D.Double playerBounds = new Rectangle2D.Double(thePlayer.getX() + thePlayer.getTileSize(), thePlayer.getY() + thePlayer.getTileSize(), thePlayer.getTileSize(), thePlayer.getTileSize());
-        Rectangle2D.Double doorBounds = new Rectangle2D.Double(myX, myY, DOOR_WIDTH, DOOR_HEIGHT);
+        Rectangle2D.Double playerBounds = new Rectangle2D.Double(thePlayer.getX() + thePlayer.getTileSize(), thePlayer.getY() + thePlayer.getTileSize(), thePlayer.getTileSize(), thePlayer.getTileSize() * 2);
+        Rectangle2D.Double doorBounds;
+
+        switch (myDirection) {
+            case DoorDirection.UP:
+                doorBounds = new Rectangle2D.Double(myX , myY, TILE_DIMENSION, (double) TILE_DIMENSION);
+                break;
+            case DoorDirection.DOWN:
+                doorBounds = new Rectangle2D.Double(myX, myY + 24, TILE_DIMENSION, (double) TILE_DIMENSION / 2);
+                break;
+            case DoorDirection.LEFT:
+                doorBounds = new Rectangle2D.Double(myX, myY, (double) TILE_DIMENSION / 4, TILE_DIMENSION);
+                break;
+            case DoorDirection.RIGHT:
+                doorBounds = new Rectangle2D.Double(myX + 24, myY, (double) TILE_DIMENSION / 4, TILE_DIMENSION);
+                break;
+            default:
+                doorBounds = new Rectangle2D.Double(myX, myY, TILE_DIMENSION, TILE_DIMENSION);
+        }
+
         return playerBounds.intersects(doorBounds);
     }
 }
